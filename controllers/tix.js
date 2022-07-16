3aac3fa1fba47791aa70683557a8eca06d7b4c59
root <root@iPhone>
2022-07-16 07:12:03 -0400

    new db

---
+++ controllers/tix.js
@@ -0,0 +1,39 @@
+const Tix= require ("../models/tix"); 
+const bcryptjs=require("bcryptjs");
+const jwt=require("jsonwebtoken");
+
+async function getTixsByStatus(status){
+   const tixs = await Tix.find()
+   .where({status})
+   .sort({createdAt: -1})
+   ;
+   return travels;
+}
+
+async function newTix(input){
+    const newTix = input;
+    const { 
+        client, 
+        destin, 
+        rorigin, 
+        amount,
+        distance, 
+        rateType, 
+        currency, 
+        status
+    }=newTravel;
+
+    try{
+        const tix = new Tix(newTix);
+        tix.save();
+        return tix; 
+    }
+    catch(error){
+        console.log(error);
+    }
+}
+
+module.exports={
+    newTix,
+    getTixsByStatus,
+};
--- gql/resolver.js
+++ gql/resolver.js
@@ -1,5 +1,6 @@
 const userController= require("../controllers/user");
 const travelController= require("../controllers/travel");
+const tixController= require("../controllers/tix");
 const { NoUnusedVariablesRule } = require("graphql");
 const resolvers = {
 	Query:{
@@ -8,9 +9,11 @@ const resolvers = {
 			return null;
 		},
 		getTravelsByStatus: (_,{status}) =>travelController.getTravelsByStatus(status),
+		getTixsByStatus: (_,{status}) =>tixController.getTixsByStatus(status),
 	},
 	Mutation:{
 		//user 
+		newTix:(_,{input})=> tixController.newTix(input),
 		newTravel:(_,{input})=> travelController.newTravel(input),
 		register:(_,{input})=> userController.register(input),
 		login:(_,{input})=> userController.login(input),
